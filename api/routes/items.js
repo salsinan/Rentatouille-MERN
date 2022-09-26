102b9ae0b08ee1c87fdf2dc025d670daf15b9ec8
sarah alsinan <salsinan@protonmail.com>
2022-09-26 12:41:36 -0700

    Adds items routes

--- api/routes/items.js
+++ api/routes/items.js
@@ -9,14 +9,51 @@ router.post('/', async (req, res) => {
     const newItem = new Item(req.body);
     try {
         const savedItem = await newItem.save();
-        res.send(200).json(savedItem)
+        res.status(200).json(savedItem)
     } catch (err) {
-        res.send(500).json(err)
+        res.status(500).json(err)
     }
 })
 // UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedItem = await Item.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedItem);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 // DELETE
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Item.findByIdAndDelete(id);
+        res.status(200).json(`Your item was deleted successfully`);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 // GET
+router.get('/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id)
+        res.status(200).json(item);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 // GET ALL
+router.get('/', async (req, res) => {
+    try {
+        const items = await Item.find()
+        res.status(200).json(items);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 export default router;
