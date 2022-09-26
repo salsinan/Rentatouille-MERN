40f94fb5d55a0b984819e5a6e913e69985a13d72
sarah alsinan <salsinan@protonmail.com>
2022-09-26 15:08:50 -0700

    Implements login route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,13 @@
-import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+
 
 // CREATE
 export const register = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync("B4c0/\/", salt);
+        const hash = bcrypt.hashSync(req.body.password, salt);
 
         const newUser = new User({
             username: req.body.username,
@@ -14,6 +16,24 @@ export const register = async (req, res, next) => {
         })
         await newUser.save();
         res.status(200).send("User has been created")
+    } catch (err) {
+        next(err)
+    }
+}
+
+// LOGIN
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ 
+            username: req.body.username 
+        })
+        if (!user) return next(createError(404, "Invalid username"))
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+
+        if (!isPasswordCorrect) return next(createError(400, "Invalid password"))
+
+        res.status(200).json(user);
     } catch (err) {
         next(err)
     }
