37125580c0864f3f00b9e5b3e12508232ab412e0
sarah alsinan <salsinan@protonmail.com>
2022-09-27 08:35:50 -0700

    Applies authentication and authorization checks to user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { updateUser, deleteUser, getUser, getUsers } from "../controllers/User.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("users endpoint")
-})
+// UPDATE
+router.put('/:id', verifyUser, updateUser)
+// DELETE
+router.delete('/:id', verifyUser, deleteUser)
+// GET
+router.get('/:id', verifyUser, getUser)
+// GET ALL
+router.get('/', verifyAdmin, getUsers)
 
 export default router;
---
+++ api/utils/verifyToken.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token){
+        return next(createError(401, "You are not authenticated!"))
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+        if (err) return next(createError(403, "Invalid token!"));
+        req.user = user;
+        next();
+    })
+}
+
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if((req.user.id === req.params.id) || req.user.isAdmin) {
+            next();
+        } else {
+            return next(createError(403, "Authorization failed!"))
+        }
+    });
+};
+
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin) {
+            next();
+        } else {
+            return next(createError(403, "Permission denied!"))
+        }
+    });
+};
